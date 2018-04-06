f8a1b47bc8295d89b65f33caadd9e720534db475
Jorge <barbozajorge@gmail.com>
2018-04-05 22:30:10 -0400

    Initial commit Todo App

--- app/app.jsx
+++ app/app.jsx
@@ -2,12 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
+
 //Load foundation
 $(document).foundation();
 
 require('style!css!sass!applicationStyles');
 
 ReactDOM.render(
-  <p>BoilerPlate 3</p>,
+  <TodoApp />,
   document.getElementById('app')
 );
