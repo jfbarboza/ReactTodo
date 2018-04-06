ff17867d46d878de7df676dbd4082bd9f71f1f11
Jorge <barbozajorge@gmail.com>
2018-04-05 22:54:42 -0400

    Create Todo and TodoList

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,34 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: ()=>{
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk the dog'
+        },
+        {
+          id: 2,
+          text: 'Clean the yard'
+        },
+        {
+          id: 3,
+          text: 'Wash dishes'
+        },
+        {
+          id: 4,
+          text: 'Drink beer'
+        }
+      ]
+    };
+  },
   render: function(){
+    var {todos} = this.state;
     return (
       <div>
-        TodoApp.jsx
+        <TodoList todos={todos}/>
       </div>
     )
   }
