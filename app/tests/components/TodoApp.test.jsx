82882526d66df47f5864ea807d2be1eac43fd571
Jorge <barbozajorge@gmail.com>
2018-04-06 16:13:13 -0400

    Complete handle add Todo

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -3,10 +3,22 @@ var ReactDOM = require('react-dom');
 var expect = require('expect');
 var $ = require('jQuery');
 var TestUtils = require('react-addons-test-utils');
-var Todo = require('Todo');
+var TodoApp = require('TodoApp');
 
 describe('TodoApp', ()=>{
   it('should exist', ()=>{
-    expect(Todo).toExist();
+    expect(TodoApp).toExist();
   });
+
+  it('should add todo to the todos state on handleAddTodo', ()=>{
+    var todoText = 'Buy paper';
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp />);
+
+    todoApp.setState({todos: []});
+    todoApp.handleAddTodo(todoText);
+
+    expect(todoApp.state.todos[0].text).toBe('Buy paper');
+
+  });
+
 });
